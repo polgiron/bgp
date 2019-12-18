a103c5eecc24b60e394d5d640460617d95777f85
polgiron <pol.giron@gmail.com>
2019-12-18 17:11:19 +0100

    some test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   beers = [
     {
+      colorBackground: '#C9E1F3',
+      colorTitle: '#221F20',
+      colorSubtitle: '#4C658B',
+      colorPlaceholder: '#EFF5FA',
+      title: 'CITRON GIVRÉ',
+      subtitle: 'India Pale Ale',
+      description: 'La Citron Givré est une édition limitée d\'une IPA houblonnée à cru avec une nouvelle variété de houblon de la côte Pacifique du nord ouest des Etats Unis: le houblon Lemondrop.Celui - ci apporte des arômes de citron, de thé, de menthe et de fleur d\'oranger - qui composent un rappel de la douceur de l\'été au coeur de l\'hiver.',
+      alcool: 7,
+      amertume: 75
+    },
+    {
+      colorBackground: '#4F6D80',
+      colorTitle: '#FEFAFB',
+      colorSubtitle: '#7FA8B9',
+      colorPlaceholder: '#79A6B7',
+      title: 'SMOKE ON THE WATER',
+      subtitle: 'Bière au Malt Fumé',
+      description: 'La Smoke on the Water est composée de trois malts traditionnels: le malt pils pour une base aux accents de céréales et de miel, le malt Munich pour la touche toastée et enfin une pointe de Rauchmalt délivrant des notes élégantes de hêtre fumé légères au nez comme en bouche.',
+      alcool: 6.5,
+      amertume: 65
+    },
+    {
       colorBackground: '#19171c',
       colorTitle: '#c8e5f8',
       colorSubtitle: '#e5e4a8',
@@ -16,7 +38,6 @@ export class HomeComponent implements OnInit {
       subtitle: 'Bière India Pale Ale',
       description: 'Nous avons conçu l\'IPA Citra Galactique à partir d\'une sélection de nos houblons favoris. Ils donnent à cette bière des notes explosives de citron et de fruits tropicaux, dont l\'amertume s\'éteint sur une touche finale sèche.',
       alcool: 6.5,
-      style: 'India Pale Ale',
       amertume: 65,
       image: 'https://lh3.googleusercontent.com/jsWcha5MRG1JlIIkZxunV3Wv6OOldcADVOJtYd6Ed_PEhzZ9ymGVqPrgzMa39FCaQFtnvGCkAC2sR73tATfSbovkv9XPXAk_ZwSeG6rV_gjS7w=w304'
     },
